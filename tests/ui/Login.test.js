9ea311baa2aa49c4043c129c8d05349c309d2d1a
marcnjaramillo <marcn.jaramillo@gmail.com>
2019-04-12 20:23:56 -0700

    Update test files

--- tests/ui/Login.test.js
+++ tests/ui/Login.test.js
@@ -1,8 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
 import assert from 'assert';
-import { configure, mount, shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import sinon from 'sinon';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -29,16 +28,10 @@ if (Meteor.isClient) {
       const email = 'test@test.com';
       const password = 'password123';
       const spy = sinon.spy();
-      const history = { replace: () => {}};
 
-      const wrapper = mount (
-        <MemoryRouter initialEntries={['/']} initialIndex={0}>
-          <Login history={history} loginWithPassword={spy} />
-        </MemoryRouter>
-      );
-      wrapper.find(Login).instance().refs['email'].value = email;
-      wrapper.find(Login).instance().refs['password'].value = password;
-      wrapper.find('form').simulate('submit');
+      const wrapper = shallow(<Login loginWithPassword={spy} />);
+      wrapper.setState({ email, password });
+      wrapper.find('form').simulate('submit', { preventDefault: () => { } });
 
       assert.deepStrictEqual(spy.getCalls()[0].args[0], { email });
       assert.deepStrictEqual(spy.getCalls()[0].args[1], password);
@@ -46,18 +39,14 @@ if (Meteor.isClient) {
 
     it('should set loginWithPassword callback errors', function () {
       const spy = sinon.spy();
-      const history = { replace: () => {}};
-      const wrapper = mount (
-        <MemoryRouter initialEntries={['/']} initialIndex={0}>
-          <Login history={history} loginWithPassword={spy} />
-        </MemoryRouter>
-      );
-      wrapper.find('form').simulate('submit');
+      const wrapper = shallow(<Login loginWithPassword={spy} />);
+
+      wrapper.find('form').simulate('submit', { preventDefault: () => { } });
       spy.getCalls()[0].args[2]({error: 'error'});
-      assert.notStrictEqual(wrapper.find(Login).instance().state, {});
+      assert.notStrictEqual(wrapper.state('error').length, 0);
       
       spy.getCalls()[0].args[2]();
-      assert.deepStrictEqual(wrapper.find(Login).instance().state, {error: ''});
+      assert.strictEqual(wrapper.state('error').length, 0);
     });
 
   });
--- tests/ui/Signup.test.js
+++ tests/ui/Signup.test.js
@@ -1,8 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
 import assert from 'assert';
-import { configure, mount, shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import sinon from 'sinon';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -16,7 +15,7 @@ if (Meteor.isClient) {
     it('should show error messages', function () {
       const error = 'This is not working';
       const history = { replace: () => {}};
-      const wrapper = shallow(<Signup history={history} createUser={() => {}}/>);
+      const wrapper = shallow(<Signup history={history} createUser={() => { }}/>);
 
       wrapper.setState({ error });
       assert.strictEqual(wrapper.find('p').text(), error);
@@ -29,56 +28,48 @@ if (Meteor.isClient) {
       const email = 'test@test.com';
       const password = 'password123';
       const spy = sinon.spy();
-      const history = { replace: () => {}};
 
-      const wrapper = mount (
-        <MemoryRouter initialEntries={['/']} initialIndex={0}>
-          <Signup history={history} createUser={spy} />
-        </MemoryRouter>
-      );
-      wrapper.find(Signup).instance().refs['email'].value = email;
-      wrapper.find(Signup).instance().refs['password'].value = password;
-      wrapper.find('form').simulate('submit');
+      const wrapper = shallow(<Signup createUser={spy} />);
+      wrapper.setState({ email, password });
+      wrapper.find('form').simulate('submit', { preventDefault: () => { } });
 
       assert.deepStrictEqual(spy.getCalls()[0].args[0], { email, password });
     });
 
     it('should show error message for short password', function () {
       const email = 'test@test.com';
-      const password = 'password123';
+      const password = 'password';
       const spy = sinon.spy();
-      const history = { replace: () => {}};
 
-      const wrapper = mount (
-        <MemoryRouter initialEntries={['/']} initialIndex={0}>
-          <Signup history={history} createUser={spy} />
-        </MemoryRouter>
-      );
-      wrapper.find(Signup).instance().refs['email'].value = email;
-      wrapper.find(Signup).instance().refs['password'].value = password;
-      wrapper.find('form').simulate('submit');
+      const wrapper = shallow(<Signup createUser={spy} />);
+      wrapper.find({ name: 'email' }).simulate('change', {
+        target: {
+          value: email
+        }
+      });
+      wrapper.find({name: 'password'}).simulate('change', {
+        target: {
+          value: password
+        }
+      });
+      wrapper.find('form').simulate('submit', { preventDefault: () => { } });
 
-      assert.notStrictEqual(wrapper.find(Signup).instance().state, {});
+      assert(wrapper.state('error').length > 0);
     });
 
     it('should set loginWithPassword callback errors', function () {
       const password = 'password123!';
       const reason = 'test reason';
       const spy = sinon.spy();
-      const history = { replace: () => {}};
-      const wrapper = mount (
-        <MemoryRouter initialEntries={['/']} initialIndex={0}>
-          <Signup history={history} createUser={spy} />
-        </MemoryRouter>
-      );
+      const wrapper = shallow(<Signup createUser={spy} />);
 
-      wrapper.find(Signup).instance().refs['password'].value = password;
-      wrapper.find('form').simulate('submit');
+      wrapper.setState({ password })
+      wrapper.find('form').simulate('submit', { preventDefault: () => { } });
       spy.getCalls()[0].args[1]({ reason });
-      assert.deepStrictEqual(wrapper.find(Signup).instance().state, {error: reason});
+      assert.deepStrictEqual(wrapper.state('error'), reason);
       
       spy.getCalls()[0].args[1]();
-      assert.deepStrictEqual(wrapper.find(Signup).instance().state, {error: ''});
+      assert.strictEqual(wrapper.state('error').length, 0);
     });
 
   });
